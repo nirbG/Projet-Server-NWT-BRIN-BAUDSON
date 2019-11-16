ac97e461742d712354ba0cd0e8d559096a89f3e1
baudson3u <lucas.baudson@hotmail.fr>
2019-11-16 17:06:58 +0100

    Initialisation de Swagger

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,31 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import * as Config from 'config';
 import { AppConfig } from './app-config.interface';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {ComicsModule} from "./comics/comics.module";
+import {HerosModule} from "./heros/heros.module";
 
 async function bootstrap(config: AppConfig) {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+
+  const options = new DocumentBuilder()
+      .setTitle('titre temporaire')
+      .setDescription('description temporaire')
+      .setVersion('1.0')
+      .addTag('tag temporaire')
+      .build();
+  // create swagger document
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [ ComicsModule, HerosModule ],
+  });
+
+  // setup swagger module
+  SwaggerModule.setup('temp', app, document);
+
   app.enableCors();
   await app.listen(config.port, config.host);
   Logger.log(`Application served at http://${config.host}:${config.port}`, 'bootstrap');
